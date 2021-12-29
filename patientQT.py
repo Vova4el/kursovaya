e6dfd56162270bb212503d9caf63496b72b9230d
Vova4el <voff.chernih@yandex.ru>
2021-12-29 12:41:55 +0300

    добавил окно пациента, реализовал фишечку с записью на приём автоматически

---
+++ patientQT.py
@@ -0,0 +1,171 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+class patientUI:
+    def __init__(self):
+        self.ui = None
+        self.prov = 0
+        self.transaction = None
+        self.textBrowser = None
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.centralwidget = None
+        self.tableWidget = None
+        self.widget = None
+        self.gridLayout = None
+        self.label_4 = None
+        self.label_6 = None
+        self.textEdit = None
+        self.pushButton_2 = None
+        self.label_2 = None
+        self.comboBox_2 = None
+        self.label_7 = None
+        self.pushButton_5 = None
+        self.pushButton_3 = None
+        self.comboBox = None
+        self.label_5 = None
+        self.label_3 = None
+        self.label_8 = None
+        self.comboBox_4 = None
+        self.pushButton_4 = None
+        self.comboBox_3 = None
+        self.pushButton = None
+        self.comboBox_5 = None
+        self.menubar = None
+        self.statusbar = None
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(907, 658)
+        MainWindow.setMinimumSize(QtCore.QSize(907, 658))
+        MainWindow.setMaximumSize(QtCore.QSize(907, 658))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 0, 891, 451))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(10, 460, 888, 156))
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_4 = QtWidgets.QLabel(self.widget)
+        self.label_4.setMinimumSize(QtCore.QSize(200, 20))
+        self.label_4.setMaximumSize(QtCore.QSize(200, 20))
+        self.label_4.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 0, 0, 1, 1)
+        self.label_6 = QtWidgets.QLabel(self.widget)
+        self.label_6.setMinimumSize(QtCore.QSize(170, 20))
+        self.label_6.setMaximumSize(QtCore.QSize(300, 20))
+        self.label_6.setObjectName("label_6")
+        self.gridLayout.addWidget(self.label_6, 0, 1, 1, 1)
+        self.textEdit = QtWidgets.QTextEdit(self.widget)
+        self.textEdit.setMinimumSize(QtCore.QSize(200, 40))
+        self.textEdit.setMaximumSize(QtCore.QSize(200, 40))
+        self.textEdit.setObjectName("textEdit")
+        self.gridLayout.addWidget(self.textEdit, 1, 0, 3, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_2.setMinimumSize(QtCore.QSize(200, 30))
+        self.pushButton_2.setMaximumSize(QtCore.QSize(200, 30))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 1, 1, 2, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_3.setMinimumSize(QtCore.QSize(200, 30))
+        self.pushButton_3.setMaximumSize(QtCore.QSize(200, 30))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 3, 1, 2, 1)
+        self.comboBox_3 = QtWidgets.QComboBox(self.widget)
+        self.comboBox_3.setMinimumSize(QtCore.QSize(150, 30))
+        self.comboBox_3.setMaximumSize(QtCore.QSize(150, 30))
+        self.comboBox_3.setObjectName("comboBox_3")
+        self.gridLayout.addWidget(self.comboBox_3, 3, 6, 2, 1)
+        self.label_5 = QtWidgets.QLabel(self.widget)
+        self.label_5.setMinimumSize(QtCore.QSize(200, 20))
+        self.label_5.setMaximumSize(QtCore.QSize(200, 20))
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
+        self.comboBox = QtWidgets.QComboBox(self.widget)
+        self.comboBox.setMinimumSize(QtCore.QSize(150, 30))
+        self.comboBox.setMaximumSize(QtCore.QSize(150, 30))
+        self.comboBox.setObjectName("comboBox")
+        self.gridLayout.addWidget(self.comboBox, 4, 2, 2, 2)
+        self.comboBox_5 = QtWidgets.QComboBox(self.widget)
+        self.comboBox_5.setMinimumSize(QtCore.QSize(150, 30))
+        self.comboBox_5.setMaximumSize(QtCore.QSize(150, 30))
+        self.comboBox_5.setObjectName("comboBox_5")
+        self.gridLayout.addWidget(self.comboBox_5, 5, 4, 2, 1)
+        self.pushButton_5 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_5.setMinimumSize(QtCore.QSize(150, 30))
+        self.pushButton_5.setMaximumSize(QtCore.QSize(150, 30))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.gridLayout.addWidget(self.pushButton_5, 5, 5, 2, 2)
+        self.comboBox_4 = QtWidgets.QComboBox(self.widget)
+        self.comboBox_4.setMinimumSize(QtCore.QSize(200, 30))
+        self.comboBox_4.setMaximumSize(QtCore.QSize(200, 30))
+        self.comboBox_4.setObjectName("comboBox_4")
+        self.gridLayout.addWidget(self.comboBox_4, 6, 0, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_4.setMinimumSize(QtCore.QSize(200, 30))
+        self.pushButton_4.setMaximumSize(QtCore.QSize(200, 30))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout.addWidget(self.pushButton_4, 6, 1, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setMinimumSize(QtCore.QSize(150, 30))
+        self.pushButton.setMaximumSize(QtCore.QSize(150, 30))
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 6, 2, 1, 2)
+        self.label_8 = QtWidgets.QLabel(self.widget)
+        self.label_8.setMinimumSize(QtCore.QSize(150, 20))
+        self.label_8.setMaximumSize(QtCore.QSize(200, 20))
+        self.label_8.setObjectName("label_8")
+        self.gridLayout.addWidget(self.label_8, 4, 4, 1, 1)
+        self.label_7 = QtWidgets.QLabel(self.widget)
+        self.label_7.setMinimumSize(QtCore.QSize(150, 20))
+        self.label_7.setMaximumSize(QtCore.QSize(150, 20))
+        self.label_7.setObjectName("label_7")
+        self.gridLayout.addWidget(self.label_7, 3, 2, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        self.label_3.setMinimumSize(QtCore.QSize(150, 20))
+        self.label_3.setMaximumSize(QtCore.QSize(150, 20))
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 2, 6, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 907, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Окно пациента"))
+        self.label_4.setText(_translate("MainWindow", "Поле ввода"))
+        self.label_6.setText(_translate("MainWindow", "Сначала откройте расписание приёмов"))
+        self.pushButton_2.setText(_translate("MainWindow", "Изменить таблицу"))
+        self.pushButton_3.setText(_translate("MainWindow", "Подтвердить изменения"))
+        self.label_5.setText(_translate("MainWindow", "Выберите как изменить таблицу"))
+        self.pushButton_5.setText(_translate("MainWindow", "Автозапись к опр. спец"))
+        self.pushButton_4.setText(_translate("MainWindow", "Отменить изменения"))
+        self.pushButton.setText(_translate("MainWindow", "Вывод таблицы"))
+        self.label_8.setText(_translate("MainWindow", "Выберите ID специализации"))
+        self.label_7.setText(_translate("MainWindow", "Выберите таблицу"))
+        self.label_3.setText(_translate("MainWindow", "Выберите № ID из таблицы"))
+        self.comboBox.addItem("Пациенты")
+        self.comboBox.addItem("День")
+        self.comboBox.addItem("Специализация")
+        self.comboBox.addItem("Статус приёма")
+        self.comboBox.addItem("Кабинеты")
+        self.comboBox.addItem("Персонал")
+        self.comboBox.addItem("Время работы")
+        self.comboBox.addItem("Приём")
+        self.comboBox.addItem("Мед книжка")
+        self.comboBox_4.addItem("Добавить строку")
+        self.comboBox_4.addItem("Отменить запись на приём")
+        self.comboBox_4.addItem("Обновить элемент")
--- window_class.py
+++ window_class.py
@@ -1,12 +1,15 @@
-
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
+from PyQt5.QtWidgets import QTableWidgetItem
 from classes import *
-from datetime import datetime, date, time
 from Qt import *
 from Authentication import login_panell
 import classes
 from personalQT import personalUI
+from patientQT import patientUI
+from datetime import timedelta,datetime, date, time
 
-
+################ОКНО_АДМИНА##########################
 class admin_panel(QMainWindow):
     def __init__(self, parent=None):
         super(admin_panel, self).__init__(parent)
@@ -597,6 +600,241 @@ class personal_panel(QMainWindow):
         else:
             self.ui.statusbar.showMessage("Сначала внесите изменение в таблицу")
 
+
+
+#################################ОКНО_ПАЦИЕНТА######################################
+class patient_panel(QMainWindow):
+    def __init__(self, parent=None):
+        super(patient_panel, self).__init__(parent)
+        self.ui = patientUI()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(lambda: self.write_table())
+        self.ui.pushButton_2.clicked.connect(lambda: self.change_table())
+        self.ui.pushButton_3.clicked.connect(lambda: self.confirm_change())
+        self.ui.pushButton_4.clicked.connect(lambda: self.undo_change())
+        self.ui.pushButton_5.clicked.connect(lambda: self.autoplay_spec())
+
+
+    def write_table(self):
+        self.ui.comboBox_3.clear()
+        self.ui.tableWidget.clearSelection()
+        result = self.ui.comboBox.currentText()
+        line = 0
+        collums = []
+        if result == "Пациенты":
+            table = patient
+            self.ui.tableWidget.setColumnCount(3)
+            collums = ['id', 'name', 'id_acc']
+            self.ui.tableWidget.setHorizontalHeaderLabels(["ID", "ФИО", "ID аккаунта"])
+        if result == "День":
+            table = day
+            self.ui.tableWidget.setColumnCount(2)
+            collums = ['id', 'name']
+            self.ui.tableWidget.setHorizontalHeaderLabels(
+                ["ID", "День"])
+        if result == "Специализация":
+            table = specialization
+            self.ui.tableWidget.setColumnCount(2)
+            collums = ['id', 'name_spec']
+            self.ui.tableWidget.setHorizontalHeaderLabels(["ID", "Специализация"])
+        if result == "Статус приёма":
+            table = reception_status
+            self.ui.tableWidget.setColumnCount(2)
+            collums = ['id', 'name']
+            self.ui.tableWidget.setHorizontalHeaderLabels(["ID", "Статус"])
+        if result == "Кабинеты":
+            table = offices
+            self.ui.tableWidget.setColumnCount(2)
+            collums = ['id', 'cab_num']
+            self.ui.tableWidget.setHorizontalHeaderLabels(["ID", "Номер кабинета"])
+        if result == "Персонал":
+            table = personnel
+            self.ui.tableWidget.setColumnCount(5)
+            collums = ['id', 'id_spec', 'id_office', 'name', 'id_acc']
+            self.ui.tableWidget.setHorizontalHeaderLabels(
+                ["ID", "ID специализации", "ID кабинета", "ФИО", 'ID аккаунта'])
+        if result == "Время работы":
+            table = working_hours
+            self.ui.tableWidget.setColumnCount(6)
+            collums = ['id', 'id_day', 'id_spec', 'work_hours', 'free_time', 'break_time']
+            self.ui.tableWidget.setHorizontalHeaderLabels(
+                ["ID", "ID дня", "ID специализации", "рабочее время", "свободное время", "перерыв"])
+        if result == "Приём":
+            table = reception
+            self.ui.tableWidget.setColumnCount(7)
+            collums = ['id', 'id_patient', 'id_office', 'id_reception_status', 'id_personnel', 'id_spec', 'date']
+            self.ui.tableWidget.setHorizontalHeaderLabels(
+                ["ID", "ID пациента", "ID кабинета", "ID статуса приёма", "ID персонала", "id специализации", "Дата"])
+        if result == "Мед книжка":
+            table = med_knigа
+            self.ui.tableWidget.setColumnCount(4)
+            collums = ['id', 'id_patient', 'id_personnel', 'diagnoz']
+            self.ui.tableWidget.setHorizontalHeaderLabels(["ID", "ID пациента", "ID персонала", "Диагноз"])
+        line = session.query(table).count()
+        self.ui.qTable = session.query(table).all()
+        self.ui.tableWidget.setRowCount(line)
+        sort = [0] * line
+        j = 0
+        p = session.query(specialization).all()
+        for i in range(session.query(specialization).count()):
+            self.ui.comboBox_5.addItem(str(p[i].id))
+        for i in range(line):
+            self.ui.comboBox_3.addItem(str(i + 1))
+        for row, form in enumerate(self.ui.qTable):
+            col = 0
+            for c in collums:
+                for k, v in vars(form).items():
+                    if c == k:
+                        self.ui.tableWidget.setItem(row, col, QTableWidgetItem(str(v)))
+                        col += 1
+        for i in session.query(table).all():
+            sort[j] = i.id
+            j += 1
+        self.ui.comboBox_3.clear()
+        sort.sort()
+        for i in sort:
+            self.ui.comboBox_3.addItem(str(i))
+        perem = 0
+        j = -1
+        for i in sort:
+            j += 1
+            for row, form in enumerate(self.ui.qTable):
+                col = 0
+                for c in collums:
+                    for k, v in vars(form).items():
+                        if c == k:
+                            if c == 'id':
+                                perem = v
+                            if ((c == 'id') & (v == i)) | ((c != 'id') & (perem == i)):
+                                self.ui.tableWidget.setItem(j, col, QTableWidgetItem(str(v)))
+                                col += 1
+        self.ui.tableWidget.resizeColumnsToContents()
+
+    def change_table(self):
+        self.ui.statusbar.clearMessage()
+        if self.ui.prov == 0:
+            self.ui.prov = 1
+        change = self.ui.comboBox_4.currentText()
+        stlb = int(self.ui.comboBox_3.currentText())
+        text = self.ui.textEdit.toPlainText()
+        lis = text.split(',')
+        try:
+            if change == "Добавить строку":
+                line = session.query(reception).count()
+                tabl = session.query(reception).all()
+                f = 0  # флаг
+                free_id = 1  # свободный индекс
+                while f == 0:
+                    for i in range(line):
+                        if free_id == tabl[i].id:
+                            f = 1
+                    if f == 1:
+                        free_id = free_id + 1
+                        f = 0
+                    else:
+                        f = 1
+                pers=session.query(personnel).filter(personnel.id==int(list[0])).first()
+                new = reception(id=free_id, id_patient=classes.glob_id, id_office=pers.id_office, id_reception_status=2, id_personnel=int(lis[0]), id_spec=pers.id_spec, date=lis[1])
+                session.add(new)
+            if change == "Отменить запись на приём":
+                query = session.query(reception).get(stlb)
+                if classes.glob_id == query.id_patient:
+                    query.id_reception_status = 3
+                else:
+                    self.ui.statusbar.showMessage("Вы не можете отменить чужую запись на приём")
+            self.write_table()
+
+        except:
+            self.ui.statusbar.showMessage("Произошла ошибка")
+
+    def autoplay_spec(self):
+        if self.ui.prov == 0:
+            self.ui.prov = 1
+        spec_id = int(self.ui.comboBox_5.currentText())
+        dt = datetime.now()
+        t2 = time(0)
+        dt= datetime.combine(dt.date(),t2)
+        while datetime.now()>=dt:
+            dt = dt + timedelta(hours=1)
+        tabl = session.query(reception).filter(reception.id_spec== spec_id)
+        line = session.query(reception).filter(reception.id_spec== spec_id).count()
+        f=0
+        id_per = 0
+        while f==0:
+            f2=0
+            while f2==0:
+                for i in range(line):
+                    print(f'dt=',dt,f'datetime.strptime(tabl[i].date,%Y-%m-%d %H:%M:%S)',datetime.strptime(tabl[i].date,'%Y-%m-%d %H:%M:%S'))
+                    if dt==datetime.strptime(tabl[i].date,'%Y-%m-%d %H:%M:%S'):
+                        f2=1
+                if f2==1:
+                    f2=0
+                    dt = dt + timedelta(hours=1)
+                else:
+                    f2=1
+            wh = session.query(working_hours).filter(working_hours.id_spec == spec_id,working_hours.id_day==datetime.isoweekday(dt))
+            line_wh = session.query(working_hours).filter(working_hours.id_spec == spec_id,working_hours.id_day==datetime.isoweekday(dt)).count()
+            if line_wh!=0:
+                for j in range(line_wh):
+                    wh2=datetime.strptime(wh[j].work_hours[1],'%H:%M:%S')
+                    wh1=datetime.strptime(wh[j].work_hours[0],'%H:%M:%S')
+                    bt1 = datetime.strptime(wh[j].break_time[0], '%H:%M:%S')
+                    bt2 = datetime.strptime(wh[j].break_time[1], '%H:%M:%S')
+                    if (wh1.time()<=dt.time())&(wh2.time()>=dt.time())&((bt1.time()>dt.time())|(bt2.time()<dt.time())):
+                        print(1)
+                        l1 = session.query(reception).filter(reception.id==tabl[i].id_personnel).first()
+                        print(2)
+                        print(l1.date)
+                        l2 = session.query(reception).filter(reception.id_office==l1.id_office,datetime.strptime(l1.date,'%Y-%m-%d %H:%M:%S')==dt).count()
+                        print(2)
+                        if l2 == 0:
+                            res =dt
+                            d_per = tabl[i].id_personnel
+                            n_c = tabl[i].id_office
+                            f=1
+            dt = dt + timedelta(hours=1)
+        tabl = session.query(reception).all()
+        line = session.query(reception).count()
+        f = 0  # флаг
+        free_id = 1  # свободный индекс
+        while f == 0:
+            for i in range(line):
+                if free_id == tabl[i].id:
+                    f = 1
+            if f == 1:
+                free_id = free_id + 1
+                f = 0
+            else:
+                f = 1
+        new = reception(id=free_id, id_patient=classes.glob_id, id_office=n_c, id_reception_status=1, id_personnel=d_per, id_spec=spec_id, date=str(res))
+        session.add(new)
+        self.write_table()
+    def confirm_change(self):
+        if self.ui.prov != 0:
+            session.commit()
+            self.ui.prov = 0
+            self.write_table()
+        else:
+            self.ui.statusbar.showMessage("Сначала внесите изменение в таблицу")
+
+    def confirm_change(self):
+        if self.ui.prov != 0:
+            session.commit()
+            self.ui.prov = 0
+            self.write_table()
+        else:
+            self.ui.statusbar.showMessage("Сначала внесите изменение в таблицу")
+
+    def undo_change(self):
+        if self.ui.prov != 0:
+            session.rollback()
+            self.ui.prov = 0
+            self.write_table()
+        else:
+            self.ui.statusbar.showMessage("Сначала внесите изменение в таблицу")
+
+
+
 class log_panel(QMainWindow):
     def __init__(self, parent=None):
         super(log_panel, self).__init__(parent)
@@ -620,6 +858,14 @@ class log_panel(QMainWindow):
                     self.hide()
                     if i.id_stat == 1:
                         dialog = admin_panel(parent=self)
+                        dialog.show()
+                    if i.id_stat == 2:
+                        j = session.query(accounts).filter(accounts.login== log).first()
+                        print(j.id)
+                        p = session.query(patient).filter(patient.id_acc== j.id).first()
+                        classes.glob_id= p.id
+                        print(classes.glob_id)
+                        dialog = patient_panel(parent=self)
                         dialog.show()
                     if i.id_stat == 3:
                         j = session.query(accounts).filter(accounts.login== log).first()
@@ -667,6 +913,30 @@ class log_panel(QMainWindow):
             self.hide()
             dialog = admin_panel(parent=self)
             dialog.show()
+        if n_id == 2:
+            user_setting = accounts(id=free_id, login=log, password=pasw, id_stat=2)
+            session.add(user_setting)
+            session.commit()
+            f = 0  # флаг
+            line = session.query(patient).count()
+            tabl = session.query(patient).all()
+            free_id2 = 1  # свободный индекс
+            while f == 0:
+                for i in range(line):
+                    if free_id2 == tabl[i].id:
+                        f = 1
+                if f == 1:
+                    free_id2 = free_id2 + 1
+                    f = 0
+                else:
+                    f = 1
+            user_setting = patient(id=free_id2, name=self.ui.textEdit_3.toPlainText(), id_acc=free_id)
+            session.add(user_setting)
+            session.commit()
+            self.hide()
+            dialog = patient_panel(parent=self)
+            dialog.show()
+
         if n_id == 3:
             user_setting = accounts(id=free_id, login=log, password=pasw, id_stat=3)
             session.add(user_setting)
@@ -684,9 +954,7 @@ class log_panel(QMainWindow):
                     f = 0
                 else:
                     f = 1
-            print(f'of=',int(self.ui.comboBox_3.currentText()))
-            print(f'spec=', int(self.ui.comboBox_2.currentText()))
-            user_setting = personnel(id=free_id2, id_spec=int(self.ui.comboBox_2.currentText()), id_office= int(self.ui.comboBox_3.currentText()),name=self.ui.textEdit_3.toPlainText(), id_acc=free_id) #id,id_spec,id_office, name,id_acc
+            user_setting = personnel(id=free_id2, id_spec=int(self.ui.comboBox_2.currentText()), id_office= int(self.ui.comboBox_3.currentText()),name=self.ui.textEdit_3.toPlainText(), id_acc=free_id)
             session.add(user_setting)
             session.commit()
             self.hide()
